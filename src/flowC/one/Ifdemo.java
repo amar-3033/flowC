03f5af18b4c8155c845ac3172d7304c944b2f0f2
a <a@amars-mbp.attlocal.net>
2021-05-29 17:49:29 -0500

    first commit

---
+++ src/flowC/one/Ifdemo.java
@@ -0,0 +1,27 @@
+package flowC.one;
+
+public class Ifdemo {
+	
+	
+	
+	static void add() {
+		
+		int a=1;
+		int b =2;
+	
+	int add = a+b;
+	System.out.println(add);
+		
+	}
+	
+
+	public static void main( String args[]) {
+		
+		add();
+		
+		
+	}
+	}
+	
+
+	
